e464a909ebed23c9b5110993cc4cd28b55d58ee8
Nadia Aebischer <nadia.aebischer@unifr.ch>
2015-11-16 16:17:16 +0100

    updated login view with unifr bootstrap

--- login.php
+++ login.php
@@ -2,38 +2,35 @@
 session_name('prio2010_Beta');
 session_start();
 include("includes/fonctions.inc");
-include("header.php");
+include("header_bootstrap.php");
 
 ?>
-	<!-- start:colonneRight -->
-	<div id="content">
-	<p><h1>Administration</h1></p>
 
-	<form action="login_admin.php?idid=<?php echo($_GET['idid']);?>" method="post" name="loginForm">
-	<table cellpadding="3" border="0" width="100%" align="left">
-		<tr>
-			<td>
-				<table border="0" width="95%"cellpadding="3">
+<div class="row">
+	<div class="col-xs-12">
+		<div class="page-header">
+			<h1>Administration</h1>
+		</div>
 
-					<tr>
-						<td  valign="top" width="25%"><img src="includes/images/security.png" alt="auteur" /></td>
-						<td>Username <br />
-							<input class="form" name="username" type="text" size="25" /><br />
-							Password <br />
-							<input class="form" name="password" type="password" size="25" /><br /><br />
-							<input type="submit" name="submit" class="button" value="Login" /><br /><br />
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-	</table>
-	</form>
+		<div class="row">
+			<div class="col-xs-4">
+				<form class="form" action="login_admin.php?idid=<?php echo($_GET['idid']);?>" method="post" name="loginForm">
+					<div class="form-group">
+						<label for="username">Username </label>
+						<input type="text" class="form-control" id="username" name="username" size="25" />
+						<label for="password">Password </label>
+						<input type="text" class="form-control" id="password" name="password" size="25" />
+					</div>
+					<button type="submit" name="submit" value="Login" class="btn btn-primary">Login</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
 <script>
 	document.loginForm.username.focus();
 </script>
- 	</div>
-	<!-- end:colonneRight -->
 <?php
-include("footer.php");
+include("footer1_bootstrap.php");
 ?>
