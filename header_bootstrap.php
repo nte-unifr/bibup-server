49b11ad27d57fabf6ace3981e11c4d92a8221a93
Nadia Aebischer <nadia.aebischer@unifr.ch>
2015-12-02 09:04:40 +0100

    fixed issue with unapi server (should appear first in header)

--- header_bootstrap.php
+++ header_bootstrap.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="unapi-server" type="application/xml" title="unAPI" href="http://elearning.unifr.ch/bibup/unapi.php" />
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="keywords" lang="fr" content="universit&eacute;, formation, suisse, fribourg, recherche, &eacute;tudes" />
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 	<title><?=$titre_projet?>  - University of Fribourg</title>
-	<link rel="unapi-server" type="application/xml" title="unAPI" href="http://elearning.unifr.ch/bibup/unapi.php" />
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 
 	<!-- Open Sans from Google fonts -->
