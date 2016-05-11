335730bf9c3c3fa74d51230f9b7d2741dc052140
Alrick <alrick.deillon@unifr.ch>
2016-05-11 14:13:01 +0200

    add a location tag for each url

--- includes/fonctions_biblio.php
+++ includes/fonctions_biblio.php
@@ -229,17 +229,15 @@ function mods_from_json_data_isbn($data) {
 	$mods .= '<name type="personal"><namePart>' . XMLClean($data->list[0]->author) . '</namePart>	</name>';
 	$mods .= '<identifier type="isbn">' . isbn13to10($data->list[0]->isbn[0]) . '</identifier>';
 	$mods .= '<originInfo><publisher>' . XMLClean($data->list[0]->publisher) . '</publisher><copyrightDate>' . XMLClean($data->list[0]->year) . '</copyrightDate><edition>' . XMLClean($data->list[0]->ed) . '</edition></originInfo>';
-	$mods .= '<location>';
-	$mods .= '<url displayLabel="Worldcat">'.$data->list[0]->url.'</url>';
+	$mods .= '<location><url displayLabel="Worldcat">'.$data->list[0]->url.'</url></location>';
 	if ($data->list[0]->file1 <> '') {
 		$location = file_location(substr($data->list[0]->file1,0,-11));
-		$mods .= '<url displayLabel="Extract" access="raw object">' . $location . $data->list[0]->file1 . '</url>';
+		$mods .= '<location><url displayLabel="Extract" access="raw object">' . $location . $data->list[0]->file1 . '</url></location>';
 	}
 	if ($data->list[0]->file2 <> '') {
 		$location = file_location(substr($data->list[0]->file2,0,-9));
-		$mods .= '<url displayLabel="Title" access="raw object">' . $location . $data->list[0]->file2 . '</url>';
+		$mods .= '<location><url displayLabel="Title" access="raw object">' . $location . $data->list[0]->file2 . '</url></location>';
 	}
-	$mods .= '</location>';
 	if (!empty($_POST['note'])) {
 		$mods .= '<note>Note : ' . XMLClean($_POST['note']) . '</note>';
 	}
@@ -263,17 +261,15 @@ function mods_from_json_data_issn($data) {
 	$mods .= '<identifier type="issn">' . $_POST['isbn'] . '</identifier>';
 	$mods .= '<originInfo><publisher>' . XMLClean($data->group[0]->list[0]->publisher) . '</publisher></originInfo>';
 	$mods .= '</relatedItem>';
-	$mods .= '<location>';
-	$mods .= '<url displayLabel="Worldcat">'.$data->list[0]->url.'</url>';
+	$mods .= '<location><url displayLabel="Worldcat">'.$data->list[0]->url.'</url></location>';
 	if ($data->group[0]->file1 <> '') {
 		$location = file_location(substr($data->group[0]->file1,0,-11));
-		$mods .= '<url displayLabel="Extract" access="raw object">' . $location . $data->group[0]->file1 . '</url>';
+		$mods .= '<location><url displayLabel="Extract" access="raw object">' . $location . $data->group[0]->file1 . '</url></location>';
 	}
 	if ($data->group[0]->file2 <> '') {
 		$location = file_location(substr($data->group[0]->file2,0,-9));
-		$mods .= '<url displayLabel="Title" access="raw object">' . $location . $data->group[0]->file2 . '</url>';
+		$mods .= '<location><url displayLabel="Title" access="raw object">' . $location . $data->group[0]->file2 . '</url></location>';
 	}
-	$mods .= '</location>';
 	if (!empty($_POST['note'])) {
 		$mods .= '<note>Note : ' . XMLClean($_POST['note']) . '</note>';
 	}
